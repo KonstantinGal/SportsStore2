56961916eea77cc9929d3f01ec806959b8dc1f67
Konstantin Galkin <galkink@my.erau.edu>
2019-05-28 12:23:13 -0400

    Changed ProductController

--- SportsStore2.WebUI/Controllers/ProductController.cs
+++ SportsStore2.WebUI/Controllers/ProductController.cs
@@ -23,11 +23,12 @@ namespace SportsStore2.WebUI.Controllers
             this.repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category,int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category ==null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * PageSize)
                     .Take(PageSize),
@@ -36,8 +37,8 @@ namespace SportsStore2.WebUI.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-
-                }
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
--- SportsStore2.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore2.WebUI/Models/ProductsListViewModel.cs
@@ -10,5 +10,6 @@ namespace SportsStore2.WebUI.Models
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
